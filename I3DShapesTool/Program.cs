01b6cd7be57e1fb99c2e383a9e4e030fc638029f
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2017-09-03 19:34:29 +0200

    Added decryptor code

--- I3DShapesTool/Program.cs
+++ I3DShapesTool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,47 @@ namespace I3DShapesTool
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+
+            string path = args.Length > 0 ? args[0] : @"D:\SteamLibrary\steamapps\common\Farming Simulator 2013\data\vehicles\balers\kroneBigPack1290.i3d.shapes";
+
+            using (FileStream fs = File.OpenRead(path))
+            {
+                Console.WriteLine("Loading file: " + Path.GetFileName(fs.Name));
+
+                byte seed = (byte)fs.ReadInt16L();
+                Console.WriteLine("File Seed: " + seed);
+
+                short version = fs.ReadInt16L();
+                Console.WriteLine("File Version: " + version);
+
+                if (version != 2)
+                    throw new NotSupportedException("Unsupported version");
+
+                Console.WriteLine();
+                
+                using (I3DDecryptorStream dfs = new I3DDecryptorStream(fs, seed))
+                {
+                    int itemCount = dfs.ReadInt32L();
+                    Console.WriteLine("Found " + itemCount + " items");
+                    Console.WriteLine();
+                    for (int i = 0; i < itemCount; i++)
+                    {
+                        Console.WriteLine("Loading item " + (i + 1));
+
+                        int type = dfs.ReadInt32L();
+                        Console.WriteLine("Type: " + type);
+                        int size = dfs.ReadInt32L();
+                        Console.WriteLine("Size: " + size);
+                        byte[] data = dfs.ReadBytes(size);
+                        File.WriteAllBytes(@"C:\Users\Daniel\Desktop\data\" + i, data);
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.Read();
         }
     }
 }
---
+++ I3DShapesTool/StreamExt.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+
+namespace I3DShapesTool
+{
+    /// <summary>
+    /// Created by "high" https://facepunch.com/member.php?u=60704
+    /// </summary>
+    public static class StreamExt
+    {
+        [ThreadStatic]
+        private static byte[] _buffer;
+
+        private static byte[] Buffer => _buffer ?? (_buffer = new byte[16]);
+
+        public static void FillBuffer(this Stream stream, int numBytes)
+        {
+            if (numBytes < 0x0 || numBytes > Buffer.Length)
+                throw new ArgumentOutOfRangeException(nameof(numBytes));
+            if (numBytes < 1)
+                return;
+
+            int read;
+            if (numBytes == 0x1)
+            {
+                read = stream.ReadByte();
+                if (read == -1)
+                    throw new EndOfStreamException("End of stream");
+                Buffer[0x0] = (byte) read;
+            }
+            else
+            {
+                int offset = 0x0;
+                do
+                {
+                    read = stream.Read(Buffer, offset, numBytes - offset);
+                    if (read == 0x0)
+                        throw new EndOfStreamException("End of stream");
+                    offset += read;
+                } while (offset < numBytes);
+            }
+        }
+
+        public static byte ReadInt8(this Stream s)
+        {
+            int read = s.ReadByte();
+            if (read == -1)
+                throw new EndOfStreamException("End of stream");
+            return (byte) read;
+        }
+
+        public static bool ReadBoolean(this Stream s)
+        {
+            return s.ReadInt8() != 0;
+        }
+
+        public static short ReadInt16(this Stream s)
+        {
+            s.FillBuffer(0x2);
+            return (short) (Buffer[0x0] | (Buffer[0x1] << 0x8));
+        }
+
+        public static short ReadInt16L(this Stream s)
+        {
+            s.FillBuffer(0x2);
+            return (short) (Buffer[0x1] | (Buffer[0x0] << 0x8));
+        }
+
+        public static int ReadInt32(this Stream s)
+        {
+            s.FillBuffer(0x4);
+            return Buffer[0x0] | (Buffer[0x1] << 0x8) | (Buffer[0x2] << 0x10) | (Buffer[0x3] << 0x18);
+        }
+
+        public static int ReadInt32L(this Stream s)
+        {
+            s.FillBuffer(0x4);
+            return Buffer[0x3] | (Buffer[0x2] << 0x8) | (Buffer[0x1] << 0x10) | (Buffer[0x0] << 0x18);
+        }
+
+        public static long ReadInt64(this Stream s)
+        {
+            s.FillBuffer(0x8);
+            ulong num = (uint) (Buffer[0x0] | (Buffer[0x1] << 0x8) | (Buffer[0x2] << 0x10) | (Buffer[0x3] << 0x18));
+            ulong num2 = (uint) (Buffer[0x4] | (Buffer[0x5] << 0x8) | (Buffer[0x6] << 0x10) | (Buffer[0x7] << 0x18));
+            return (long) ((num2 << 0x20) | num);
+        }
+
+        public static long ReadInt64L(this Stream s)
+        {
+            s.FillBuffer(0x8);
+            ulong num = (uint) (Buffer[0x3] | (Buffer[0x2] << 0x8) | (Buffer[0x1] << 0x10) | (Buffer[0x0] << 0x18));
+            ulong num2 = (uint) (Buffer[0x7] | (Buffer[0x6] << 0x8) | (Buffer[0x5] << 0x10) | (Buffer[0x4] << 0x18));
+            return (long) ((num << 0x20) | num2);
+        }
+        /*
+        public static unsafe double ReadDouble(this Stream s)
+        {
+            ulong ret = (ulong) s.ReadInt64();
+            return *((double*) &ret);
+        }
+
+        public static unsafe float ReadSingle(this Stream s)
+        {
+            int ret = s.ReadInt32();
+            return *((float*) &ret);
+        }
+        */
+        public static byte[] ReadBytes(this Stream s, int count)
+        {
+            if (count < 0x0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            byte[] buffer = new byte[count];
+            int offset = 0x0;
+            do
+            {
+                int num2 = s.Read(buffer, offset, count);
+                if (num2 == 0x0)
+                    break;
+                offset += num2;
+                count -= num2;
+            } while (count > 0x0);
+
+            if (offset != buffer.Length)
+            {
+                byte[] dst = new byte[offset];
+                System.Buffer.BlockCopy(buffer, 0x0, dst, 0x0, offset);
+                buffer = dst;
+            }
+
+            return buffer;
+        }
+    }
+}
