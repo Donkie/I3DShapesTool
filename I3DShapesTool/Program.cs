fd122f14949796b14549d96d24e9fc4d172706fd
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2017-09-03 18:35:06 +0200

    Add project files.

---
+++ I3DShapesTool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I3DShapesTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
