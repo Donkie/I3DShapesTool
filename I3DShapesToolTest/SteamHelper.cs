04f6bde7935b79803ede67d39ff5716b7202d88c
VirRus77 <virrus77@gmail.com>
2020-01-26 20:20:26 +0300

    Optimize SteamHelper.

--- I3DShapesToolTest/SteamHelper.cs
+++ I3DShapesToolTest/SteamHelper.cs
@@ -1,82 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.Win32;
 
 namespace I3DShapesToolTest
 {
-    class SteamHelper
+    /// <summary>
+    /// Base on: https://stackoverflow.com/questions/54767662/finding-game-launcher-executables-in-directory-c-sharp
+    /// </summary>
+    public static class SteamHelper
     {
-        private static List<string> steamGameDirs;
-        
-        public static void LoadSteamGameDirectories()
+        private static ICollection<string> RegistryKeys = new[] { "SOFTWARE\\Wow6432Node\\Valve\\", "SOFTWARE\\VALVE\\" };
+        private static ICollection<string> _steamGameDirs = new List<string>();
+
+        static SteamHelper()
         {
-            steamGameDirs = new List<string>();
-            const string steam32 = "SOFTWARE\\VALVE\\";
-            const string steam64 = "SOFTWARE\\Wow6432Node\\Valve\\";
-            var key32 = Registry.LocalMachine.OpenSubKey(steam32);
-            var key64 = Registry.LocalMachine.OpenSubKey(steam64);
-            if (key64?.ToString() == null || key64.ToString() == "")
-            {
-                foreach (var k32SubKey in key32.GetSubKeyNames())
+            UpdateSteamGameDirectories();
+        }
+
+        private static void UpdateSteamGameDirectories()
+        {
+            _steamGameDirs = new List<string>();
+            RegistryKeys.Select(v => Registry.LocalMachine.OpenSubKey(v))
+                .Where(registryKey => registryKey != null)
+                .SelectMany(registryKey =>
                 {
-                    using (var subKey = key32.OpenSubKey(k32SubKey))
+                    using (registryKey)
                     {
-                        var steam32Path = subKey?.GetValue("InstallPath").ToString();
-                        var config32Path = $"{steam32Path}/steamapps/libraryfolders.vdf";
-                        const string driveRegex = @"[A-Z]:\\";
-                        if (!File.Exists(config32Path)) continue;
-                        var configLines = File.ReadAllLines(config32Path);
-                        foreach (var item in configLines)
-                        {
-                            //Console.WriteLine($"32:  {item}");
-                            var match = Regex.Match(item, driveRegex);
-                            if (item == string.Empty || !match.Success) continue;
-                            var matched = match.ToString();
-                            var item2 = item.Substring(item.IndexOf(matched, StringComparison.Ordinal));
-                            item2 = item2.Replace("\\\\", "\\");
-                            item2 = item2.Replace("\"", "\\steamapps\\common\\");
-                            steamGameDirs.Add(item2);
-                        }
-                        steamGameDirs.Add($"{steam32Path}\\steamapps\\common\\");
+                        return GetDirectories(registryKey).ToArray();
                     }
-                }
-            }
-            foreach (var k64SubKey in key64.GetSubKeyNames())
+                })
+                .ToList()
+                .ForEach(directoryName => _steamGameDirs.Add(directoryName));
+        }
+
+        private static IEnumerable<string> GetDirectories(RegistryKey registryKey)
+        {
+            foreach (var subKeyName in registryKey.GetSubKeyNames())
             {
-                using (var subKey = key64.OpenSubKey(k64SubKey))
+                using (var subKey = registryKey.OpenSubKey(subKeyName))
                 {
-                    var steam64Path = subKey?.GetValue("InstallPath").ToString();
-                    var config64Path = $"{steam64Path}/steamapps/libraryfolders.vdf";
+                    var steamPath = subKey?.GetValue("InstallPath").ToString();
+                    var configPath = $"{steamPath}/steamapps/libraryfolders.vdf";
                     const string driveRegex = @"[A-Z]:\\";
-                    if (!File.Exists(config64Path)) continue;
-                    var configLines = File.ReadAllLines(config64Path);
+                    if (!File.Exists(configPath))
+                        continue;
+                    var configLines = File.ReadAllLines(configPath);
                     foreach (var item in configLines)
                     {
-                        //Console.WriteLine($"64:  {item}");
                         var match = Regex.Match(item, driveRegex);
                         if (item == string.Empty || !match.Success) continue;
                         var matched = match.ToString();
                         var item2 = item.Substring(item.IndexOf(matched, StringComparison.Ordinal));
                         item2 = item2.Replace("\\\\", "\\");
                         item2 = item2.Replace("\"", "\\steamapps\\common\\");
-                        steamGameDirs.Add(item2);
+                        yield return item2;
                     }
-                    steamGameDirs.Add($"{steam64Path}\\steamapps\\common\\");
+                    yield return $"{steamPath}\\steamapps\\common\\";
                 }
             }
         }
 
         public static string GetGameDirectory(string gameFolderName)
         {
-            foreach (var steamGameDir in steamGameDirs)
-            {
-                if (Directory.Exists(Path.Combine(steamGameDir, gameFolderName)))
-                    return Path.Combine(steamGameDir, gameFolderName);
-            }
-
-            return null;
+            return _steamGameDirs.Select(v => Path.Combine(v, gameFolderName))
+                .FirstOrDefault(v => Directory.Exists(v));
         }
     }
 }
