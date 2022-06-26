1ee1bed3c9ede790ddd9c2774af4a77f5fe30a57
Donkie <daniel.cf.hultgren@gmail.com>
2022-06-26 20:50:50 +0200

    Added some error checking to subset uv density stuff

--- I3DShapesTool.Lib/Model/I3DShapeSubset.cs
+++ I3DShapesTool.Lib/Model/I3DShapeSubset.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace I3DShapesTool.Lib.Model
 {
@@ -42,13 +43,22 @@ namespace I3DShapesTool.Lib.Model
 
             if(version >= 6)
             {
-                if(options.HasFlag(I3DShapeOptions.HasUV1) && UVDensity1 != null)
+                if(options.HasFlag(I3DShapeOptions.HasUV1) && UVDensity1 == null)
+                    throw new InvalidOperationException("Shape has a UV1 map but subset doesn't have any UV density 1 set.");
+                if(options.HasFlag(I3DShapeOptions.HasUV2) && UVDensity2 == null)
+                    throw new InvalidOperationException("Shape has a UV2 map but subset doesn't have any UV density 2 set.");
+                if(options.HasFlag(I3DShapeOptions.HasUV3) && UVDensity3 == null)
+                    throw new InvalidOperationException("Shape has a UV3 map but subset doesn't have any UV density 3 set.");
+                if(options.HasFlag(I3DShapeOptions.HasUV4) && UVDensity4 == null)
+                    throw new InvalidOperationException("Shape has a UV4 map but subset doesn't have any UV density 4 set.");
+
+                if(UVDensity1 != null)
                     bw.Write((float)UVDensity1);
-                if(options.HasFlag(I3DShapeOptions.HasUV2) && UVDensity2 != null)
+                if(UVDensity2 != null)
                     bw.Write((float)UVDensity2);
-                if(options.HasFlag(I3DShapeOptions.HasUV3) && UVDensity3 != null)
+                if(UVDensity3 != null)
                     bw.Write((float)UVDensity3);
-                if(options.HasFlag(I3DShapeOptions.HasUV4) && UVDensity4 != null)
+                if(UVDensity4 != null)
                     bw.Write((float)UVDensity4);
             }
         }
